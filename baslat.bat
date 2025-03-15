60acad58c81ca6ea130ef69e6e3d8ff6576c4ed1
axenh.dev <115844770+Mitsukibix@users.noreply.github.com>
2025-03-15 18:59:15 +0300

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,12 @@
+
+
+@echo off
+title DmAll Botu
+color b
+echo Bot Baslatiliyor Lutfen Bekleyiniz
+
+:baslat
+@echo off
+node .
+echo Bot Crashed, Tekrar Baslatiliyor
+goto baslat
---
+++ kurulum.bat
@@ -0,0 +1,10 @@
+@echo off
+color a
+title Dm All Botu Modul Kurulum
+
+echo config.json Dosyasini Doldurmayi Unutmayin!
+echo Doldurgunuzu onaylamak icin bir tusa basin!
+pause
+
+npm i discord.js
+
